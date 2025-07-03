be8c6bcde9e95322e6a162c5b5a4060cf0905c0a
ggomez90 <gguillermo1990@gmail.com>
2025-07-02 23:51:12 -0300

    configuracion webhook para servidor nube

--- src/app.js
+++ src/app.js
@@ -84,6 +84,23 @@ const main = async () => {
         })
     )
 
+
+    adapterProvider.server.get('/webhook', (req, res) => {
+    const VERIFY_TOKEN = process.env.VERIFY_TOKEN
+
+    const mode = req.query['hub.mode']
+    const token = req.query['hub.verify_token']
+    const challenge = req.query['hub.challenge']
+
+    if (mode && token && mode === 'subscribe' && token === VERIFY_TOKEN) {
+        console.log('WEBHOOK_VERIFIED')
+        res.status(200).send(challenge)
+    } else {
+        res.sendStatus(403)
+    }
+})
+
+
     httpServer(+PORT)
 }
 
